5e48e1d2cd794286147dc8a730860a8fe7402777
Judah Parsons <GEWDDEWD9@hotmail.com>
2017-05-30 09:33:36 +1200

    updated resources

--- SnailBettingAssessment/Properties/Resources.Designer.cs
+++ SnailBettingAssessment/Properties/Resources.Designer.cs
@@ -71,6 +71,16 @@ namespace SnailBettingAssessment.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Gary_the_Snail1 {
+            get {
+                object obj = ResourceManager.GetObject("Gary_the_Snail1", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap Snail_10 {
